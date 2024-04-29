e7801ad017d2277786acca89a5c4ab7540b4287e
eugenesvk <eugenesvk@users.noreply.github.com>
2024-04-29 18:47:08 +0700

    update alias

--- helper/src/alias.rs
+++ helper/src/alias.rs
@@ -1,8 +1,8 @@
-// pub use reexports for use in other files
-use crate::helper::*;
-pub use std          	::println as p; // requires text editor's syntax theme override to retain syntax highlighting
-pub use std::any     	::type_name; // for type_of
-pub use crate::helper	::print_type_of as pt;
+use crate::helper::*; // pub use reexports for use in other files
+pub use std          	::println      	as p 	; // requires text editor's syntax theme override to retain syntax highlighting
+pub use std          	::eprintln     	as pe	;
+pub use crate::helper	::print_type_of	as pt	;
+pub use std::any     	::type_name    	     	; // for type_of
 
 #[macro_export] macro_rules! pb { // println! during build
   ($($tokens:tt)*) => {println!("cargo:warning={}", format!($($tokens)*))}}
