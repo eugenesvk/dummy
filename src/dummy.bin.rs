50ae2b299d2679148db8cca71494d2e88369ef66
eugenesvk <eugenesvk@users.noreply.github.com>
2024-04-29 18:13:03 +0700

    update imports

--- src/dummy.bin.rs
+++ src/dummy.bin.rs
@@ -1,8 +1,9 @@
 #![allow(unused_imports,unused_variables,unreachable_code,dead_code,non_upper_case_globals)]
 extern crate helper;
-use helper        	::*; // gets macros
-use helper::alias 	::*;
-use helper::helper	::*;
+use helper            	::*; // gets macros
+pub use helper_proc   	::*; // gets proc macros
+pub use helper::alias 	::*;
+pub use helper::helper	::*;
 
 _mod!(binmod); //→ #[path="binmod/[binmod].rs"] pub mod binmod;
 use crate::binmod::print42;
