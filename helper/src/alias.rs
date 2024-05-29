1b826a733370f388642defb6669de07cb7c95fba
eugenesvk <eugenesvk@users.noreply.github.com>
2024-05-29 20:13:14 +0700

    add a writeln alias

--- helper/src/alias.rs
+++ helper/src/alias.rs
@@ -1,6 +1,7 @@
 use crate::helper::*; // pub use reexports for use in other files
 pub use std          	::println      	as p 	; // requires text editor's syntax theme override to retain syntax highlighting
 pub use std          	::eprintln     	as pe	;
+pub use std          	::writeln      	as w 	;
 pub use crate::helper	::print_type_of	as pt	;
 pub use std::any     	::type_name    	     	; // for type_of
 
